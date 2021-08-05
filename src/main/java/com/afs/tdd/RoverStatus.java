90395821c31b0f6452ed28fc432517fbf6e8d5d4
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-05 23:49:05 +0800

    [MarsRover][Mark] feat: Applied object equals checking in RoverStatus.java

--- src/main/java/com/afs/tdd/RoverStatus.java
+++ src/main/java/com/afs/tdd/RoverStatus.java
@@ -1,5 +1,7 @@
 package com.afs.tdd;
 
+import java.util.Objects;
+
 public class RoverStatus {
     private final int locationX;
     private final int locationY;
@@ -9,6 +11,19 @@ public class RoverStatus {
         this.locationX = x;
         this.locationY = y;
         this.direction = direction;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RoverStatus)) return false;
+        RoverStatus that = (RoverStatus) o;
+        return locationX == that.locationX && locationY == that.locationY && direction.equals(that.direction);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(locationX, locationY, direction);
     }
 
     public int getLocationX() {
