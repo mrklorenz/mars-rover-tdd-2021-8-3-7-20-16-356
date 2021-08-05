f8baef88f76dc24959ec6b25915f7ef7ec87990b
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-05 20:50:04 +0800

    [MarsRover][Mark] feat: Implemented method for turnLeft in MarsRover.java

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -9,6 +9,19 @@ public class MarsRover {
 
     public void executeCommand(String command) {
         if (command.equals("M")) move();
+        if (command.equals("L")) turnLeft();
+    }
+
+    private void turnLeft() {
+        final int locationX = roverStatus.getLocationX();
+        final int locationY = roverStatus.getLocationY();
+        final String direction = roverStatus.getDirection();
+        String newDirection = "N";
+
+        if (direction.endsWith("N")) newDirection = "W";
+
+
+        this.roverStatus = new RoverStatus(locationX, locationY, newDirection);
     }
 
     public void move() {
