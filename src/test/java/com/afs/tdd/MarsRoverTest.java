96acdc8e99b008d4e730a7268617dad5821b61a6
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-05 20:10:20 +0800

    [MarsRover][Mark] test: Added unit test - should_return_x0_y1_directionN_when_execute_command_given_x0_y0_directionN_commandM()

---
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -0,0 +1,25 @@
+package com.afs.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MarsRoverTest {
+
+    @Test
+    void should_return_x0_y1_directionN_when_execute_command_given_x0_y0_directionN_commandM() {
+        //given
+        MarsRover marsRover = new MarsRover(new RoverStatus(0, 0, "N"));
+        RoverStatus expectedStatus = new RoverStatus(0, 1, "N");
+
+        //when
+        marsRover.executeCommand("M");
+
+        //then
+        assertThat(expectedStatus.getLocationX()).isEqualTo(marsRover.getRoverStatus().getLocationX());
+        assertThat(expectedStatus.getLocationY()).isEqualTo(marsRover.getRoverStatus().getLocationY());
+        assertThat(expectedStatus.getDirection()).isEqualTo(marsRover.getRoverStatus().getDirection());
+    }
+
+
+}
