727a7c275086e21e69def353e4021f4d00bb55bc
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-05 23:50:01 +0800

    [MarsRover][Mark] feat: Refactored repititive codes and auto format

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -20,31 +20,26 @@ public class MarsRover {
     }
 
     public void move() {
-        final int locationX = roverStatus.getLocationX();
-        final int locationY = roverStatus.getLocationY();
-        final String direction = roverStatus.getDirection();
-        int newLocationX = locationX;
-        int newLocationY = locationY;
+        int newLocationX = roverStatus.getLocationX();
+        int newLocationY = roverStatus.getLocationY();
 
-        if (direction.equals("N")) newLocationY += 1;
-        if (direction.equals("S")) newLocationY -= 1;
-        if (direction.equals("E")) newLocationX += 1;
-        if (direction.equals("W")) newLocationX -= 1;
+        if (roverStatus.getDirection().equals("N")) newLocationY += 1;
+        if (roverStatus.getDirection().equals("S")) newLocationY -= 1;
+        if (roverStatus.getDirection().equals("E")) newLocationX += 1;
+        if (roverStatus.getDirection().equals("W")) newLocationX -= 1;
 
-        this.roverStatus = new RoverStatus(newLocationX, newLocationY, direction);
+        this.roverStatus = new RoverStatus(newLocationX, newLocationY, roverStatus.getDirection());
     }
 
     private void turnRight() {
         final int locationX = roverStatus.getLocationX();
         final int locationY = roverStatus.getLocationY();
-        final String direction = roverStatus.getDirection();
         String newDirection = "";
 
-        if (direction.equals("N")) newDirection = "E";
-        if (direction.equals("S")) newDirection = "W";
-        if (direction.equals("E")) newDirection = "S";
-        if (direction.equals("W")) newDirection = "N";
-
+        if (roverStatus.getDirection().equals("N")) newDirection = "E";
+        if (roverStatus.getDirection().equals("S")) newDirection = "W";
+        if (roverStatus.getDirection().equals("E")) newDirection = "S";
+        if (roverStatus.getDirection().equals("W")) newDirection = "N";
 
         this.roverStatus = new RoverStatus(locationX, locationY, newDirection);
     }
@@ -52,21 +47,17 @@ public class MarsRover {
     private void turnLeft() {
         final int locationX = roverStatus.getLocationX();
         final int locationY = roverStatus.getLocationY();
-        final String direction = roverStatus.getDirection();
         String newDirection = "";
 
-        if (direction.equals("N")) newDirection = "W";
-        if (direction.equals("S")) newDirection = "E";
-        if (direction.equals("E")) newDirection = "N";
-        if (direction.equals("W")) newDirection = "S";
-
+        if (roverStatus.getDirection().equals("N")) newDirection = "W";
+        if (roverStatus.getDirection().equals("S")) newDirection = "E";
+        if (roverStatus.getDirection().equals("E")) newDirection = "N";
+        if (roverStatus.getDirection().equals("W")) newDirection = "S";
 
         this.roverStatus = new RoverStatus(locationX, locationY, newDirection);
     }
 
     public RoverStatus getRoverStatus() {
-        return roverStatus;
+        return this.roverStatus;
     }
-
-
 }
